672b9a5c04ad382fb457446cec6673824b385e7e
Juan Diaz <jddiaz82@gmail.com>
2019-11-11 12:07:32 -0500

    add testimonials page

--- imports/router.js
+++ imports/router.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from './ui/Home'
 import Trips from './ui/Trips'
+import Testimonials from './ui/Testimonials'
+
 Vue.use(VueRouter)
 
 const routes = [
@@ -10,7 +12,8 @@ const routes = [
         path: '*',
         redirect: '/home'
     },
-    { path: '/trips', name: 'Trips', component: Trips }
+    { path: '/trips', name: 'Trips', component: Trips },
+    { path: '/testimonials', name: 'Testimonials', component: Testimonials }
 ]
 
 const router = new VueRouter({ mode: 'history', routes })
---
+++ imports/ui/Testimonials.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-row>
+      <v-col v-for="item in testimonial" :key="item.name" align="center" justify="center">
+        <v-card min-width="310" align="left">
+          <v-icon>fas fa-quote-left</v-icon>
+          <div class="body-1" v-text='"\""+item.description+"\""'></div>
+          <div class="font-italic font-weight-bold" align="right" v-text="'-'+item.name"></div>
+        </v-card>
+      </v-col>
+    </v-row>
+</template>
+
+<script>
+import Testimonial from "../api/Testimonial";
+export default {
+ meteor: {
+    $subscribe: {
+      testimonial: []
+    },
+    testimonial() {
+      return Testimonial.find({}, { sort: { date: -1 } });
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
